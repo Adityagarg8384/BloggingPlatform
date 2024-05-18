fff83a2302b8ce444716db8ed04679aca4bc7b3e
DevanshArora312 <devansharora312@gmail.com>
2024-05-19 00:21:31 +0530

    fixed positioning issue

--- frontend/src/Components/Assistant.jsx
+++ frontend/src/Components/Assistant.jsx
@@ -3,7 +3,7 @@ import { IoMdCloseCircleOutline } from "react-icons/io";
 const Assistant = ({state,setState}) => {
     return (
         <div>
-            <div className={`bg-red-400 text-xl absolute right-10 bottom-10 rounded-lg duration-200 ease-in-out flex justify-between items-start p-3 ${state ? "w-[15%] h-[30%]" : 'w-[0%] h-[0%]' }`}>
+            <div className={`bg-red-400 text-xl fixed right-10 bottom-10 rounded-lg duration-200 ease-in-out flex justify-between items-start p-3 ${state ? "w-[15%] h-[30%]" : 'w-[0%] h-[0%]' }`}>
                 {   
                     state && 
                     <div>
@@ -12,7 +12,7 @@ const Assistant = ({state,setState}) => {
                     </div>
                 }
             </div>
-            <button className={`rounded-full bg-blue-300 absolute right-10 bottom-10 duration-200 ease-in-out ${!state ? "w-14 h-14" : "w-0 h-0"}`} onClick={()=> setState(!state)}>
+            <button className={`rounded-full bg-blue-300 fixed right-10 bottom-10 duration-200 ease-in-out ${!state ? "w-14 h-14" : "w-0 h-0"}`} onClick={()=> setState(!state)}>
                 
             </button>
         </div>
