c0826a57002d1ac572aff77a041f5bc550e7c824
Adityagarg8384 <adityagarg8384@gmail.com>
2024-05-18 23:33:59 +0530

    Addfiles

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,25 +1,34 @@
 const mongoose= require("mongoose");
 
 const blogSchema= mongoose.Schema({
-    heading:{
+    username:{
         type:String,
         required:true,
     },
-    blogcontent:{
+    title:{
+        type:String,
+        required:true,
+    },
+    data:{
         type:String,
         required:true,
     },
     tags:[{
         type:String,
+        default:[],
     }],
     like:{
         type:Number,
         default:0,
     },
+    likeuser:[{
+        type:String,
+    }],
     comments:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'CommentSchema',
-    }]
+    }],
+   
 })
 const BlogSchema= mongoose.model("BlogSchema", blogSchema);
 module.exports= BlogSchema;
