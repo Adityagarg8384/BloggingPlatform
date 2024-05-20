4f6a5e5dd8a112dc9e5ef1dc989ac8820276a0fa
Anushka Chaudhary <138236542+anushkaa-10@users.noreply.github.com>
2024-05-20 15:49:51 +0530

    Update CreateBlog.jsx

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import TipTap from "./../Components/Tiptap";
 import DOMPurify from "dompurify";
 import { useNavigate, useParams } from "react-router-dom";
@@ -12,58 +11,49 @@ const SERVER_URL = import.meta.env.VITE_REACT_APP_SERVER_URL;
 function CreateBlog() {
 
     const [content, setContent] = useState("");
-    const [subtitle,setSubTitle] = useState("");
+    const [subtitle, setSubTitle] = useState("");
     const [heading, setHeading] = useState("");
     const [thumbnail, setThumbnail] = useState("");
     const [subtopics, setSubtopics] = useState([]);
-    const [prob, setProb]= useState(); 
-    const [secondcontent, setSecondContent]= useState("");
-    // possible scenerios
-
-    // 1. new article editor empty useParams
-
-    // 2. old draft editor non empty useParams
+    const [prob, setProb] = useState(); 
+    const [secondcontent, setSecondContent] = useState("");
 
     const blog_id = useParams().blog_id; 
     const navigate = useNavigate();
-    const [assistantState,setAssistantState] = useState(false);
-    
+    const [assistantState, setAssistantState] = useState(false);
     const [success, setSuccess] = useState();
-    const [addTags,setAddTags] = useState(false);
-    const handleTags = (flag) =>{
+    const [addTags, setAddTags] = useState(false);
+
+    const handleTags = (flag) => {
         setAddTags(false);
-        if(!flag){
+        if (!flag) {
             setSubtopics([]);
         }
-        // console.log("tags aagye:: ",subtopics);
     }
-    // Rewrite this func, abhi galt h
-    
-    const checkContent=async()=>{
+
+    const checkContent = async () => {
         const secondresponse = await fetch("http://127.0.0.1:5000/classify_ai_text", {
-            method:'POST',
+            method: 'POST',
             headers: {
                 Authorization: "Bearer your-jwt-token", // include JWT in the request header
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ text:content}),
-        })
+            body: JSON.stringify({ text: content }),
+        });
 
         const res = await secondresponse.json();
-
         setProb(res.data.probability_of_ai_text);
     }
 
     const handleSubmit = async () => {
-        // console.log(subtopics)
         const blogData = {
-            tags : subtopics,
-            data : content,
+            tags: subtopics,
+            data: content,
             title: heading,
-            subtitle : subtitle,
+            subtitle: subtitle,
             thumbnail,
         };
-        // if (blog_id) blogData.blog_id = blog_id;
+
         const token = localStorage.getItem("token");
         const response = await fetch(`${SERVER_URL}/createblog`, {
             method: "POST",
@@ -76,97 +66,88 @@ function CreateBlog() {
         if (response.ok) {
             const data = await response.json();
             if (data.success) {
-                // window.location.href =
-                // "/blogs/" + data._doc._id + "/" + data._doc.title;
-                
-                // Navigate to the blog ya profile
                 navigate("/");
             }
         }
-
-        // const secondresponse = await fetch("http://127.0.0.1:5000/classify_ai_text", {
-        //     method:'POST',
-        //     headers: {
-        //         Authorization: "Bearer your-jwt-token", // include JWT in the request header
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({ text:content}),
-        // })
-
-        // const res = await secondresponse.json();
-
-        // setProb(res.data.probability_of_ai_text);
     }
 
     return (
-    <>
-        <ResponsiveAppBar/>
-        <Assistant state={assistantState} setState={setAssistantState}/>
-        {
-            addTags ? (
-            <>
+        <>
+            <ResponsiveAppBar />
+            <Assistant state={assistantState} setState={setAssistantState} />
+            {addTags && (
                 <DialogBox
                     handleSubmit={handleTags}
                     subtopics={subtopics}
                     setSubtopics={setSubtopics}
                 />
-            </>
-            ) : (
-                <></>
-        )}
-        <div className="!h-max mt-32 lg:mt-10 ml-2">
-            <div className="mt-16 flex flex-row h-full items-center min-h-[800px] w-full !text-white">
-                <TipTap
-                    setSubTitle={setSubTitle}
-                    subtitle={subtitle}
-                    setContent={setContent}
-                    content={content}
-                    heading={heading}
-                    setHeading={setHeading}
-                    thumbnail={thumbnail}
-                    setThumbnail={setThumbnail}
-                    success={success}
-                    secondcontent={secondcontent}
-                    setSecondContent={setSecondContent}
-                />
-                <div className="flex justify-center gap-7 w-full h-full">
-                    <div
-                        className="!w-[97%] min-h-[800px] !h-full m-10 !text-black sm:mx-0  prose-h1:font-sans rounded-xl shadow-[0_0_60px_20px_rgb(0,0,0,0.22)] p-10 pt-2 prose prose-stone prose-headings:!text-black lg:prose-xl prose-img:mx-auto prose-img:rounded-xl prose-a:text-indigo-600 hover:prose-a:text-indigo-400 editor-output"
-                        dangerouslySetInnerHTML={{
-                            __html: DOMPurify.sanitize(
-                                `<h1>${heading}</h1>` + `<h3>${subtitle}</h3>` + `<img src="${thumbnail}" />` + content
-                            ),
-                        }}
-                    ></div>
+            )}
+            <div className="relative min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=100')" }}>
+                <div className="absolute inset-0 bg-black opacity-50 backdrop-blur-sm"></div>
+                <div className="relative z-10">
+                    <div className="container mx-auto mt-32 lg:mt-10 px-4">
+                        <div className="mt-16 flex flex-col lg:flex-row items-center min-h-[800px] w-full text-white">
+                            <TipTap
+                                setSubTitle={setSubTitle}
+                                subtitle={subtitle}
+                                setContent={setContent}
+                                content={content}
+                                heading={heading}
+                                setHeading={setHeading}
+                                thumbnail={thumbnail}
+                                setThumbnail={setThumbnail}
+                                success={success}
+                                secondcontent={secondcontent}
+                                setSecondContent={setSecondContent}
+                            />
+                            <div className="flex justify-center gap-7 w-full h-full">
+                                <div
+                                    className="w-full lg:w-[97%] min-h-[800px] h-full m-10 text-black sm:mx-0 prose-h1:font-sans rounded-xl shadow-[0_0_60px_20px_rgb(0,0,0,0.22)] p-10 pt-2 prose prose-stone prose-headings:text-black lg:prose-xl prose-img:mx-auto prose-img:rounded-xl prose-a:text-indigo-600 hover:prose-a:text-indigo-400 editor-output bg-white bg-opacity-90"
+                                    dangerouslySetInnerHTML={{
+                                        __html: DOMPurify.sanitize(
+                                            `<h1>${heading}</h1>` + `<h3>${subtitle}</h3>` + `<img src="${thumbnail}" />` + content
+                                        ),
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
+                        <div className="m-3 flex flex-col lg:flex-row items-center justify-center">
+                            <button
+                                type="button"
+                                className="text-white px-10 py-2 rounded-xl font-extrabold m-4 w-full lg:w-1/3 text-center justify-center"
+                                style={{ backgroundColor: '#36454F' }}
+                                onClick={() => setAddTags(true)}
+                                onMouseEnter={(e) => e.target.style.backgroundColor = '#2c3a45'}
+                                onMouseLeave={(e) => e.target.style.backgroundColor = '#36454F'}
+                            >
+                                Add Tags
+                            </button>
+                            <button
+                                type="button"
+                                className="text-white px-10 py-2 rounded-xl font-extrabold m-4 w-full lg:w-1/3 text-center justify-center"
+                                style={{ backgroundColor: '#36454F' }}
+                                onClick={handleSubmit}
+                                onMouseEnter={(e) => e.target.style.backgroundColor = '#2c3a45'}
+                                onMouseLeave={(e) => e.target.style.backgroundColor = '#36454F'}
+                            >
+                                Publish Blog
+                            </button>
+                            <button
+                                type="button"
+                                className="text-white px-10 py-2 rounded-xl font-extrabold m-4 w-full lg:w-1/3 text-center justify-center"
+                                style={{ backgroundColor: '#36454F' }}
+                                onClick={checkContent}
+                                onMouseEnter={(e) => e.target.style.backgroundColor = '#2c3a45'}
+                                onMouseLeave={(e) => e.target.style.backgroundColor = '#36454F'}
+                            >
+                                Check AI Content
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="m-3 flex items-center justify-center">
-                <button
-                type="button"
-                className="text-white bg-green-600 hover:bg-green-700 px-10 py-2 rounded-xl font-extrabold m-4 w-1/3 text-center justify-center"
-                onClick={()=> setAddTags(true)}
-                >
-                    Add Tags
-                </button>
-                <button
-                type="button"
-                className="text-white bg-green-600 hover:bg-green-700 px-10 py-2 rounded-xl font-extrabold m-4 w-1/3 text-center justify-center"
-                onClick={handleSubmit}
-                >
-                    Publish Blog
-                </button>
-                <button
-                type="button"
-                className="text-white bg-green-600 hover:bg-green-700 px-10 py-2 rounded-xl font-extrabold m-4 w-1/3 text-center justify-center"
-                onClick={checkContent}
-                >
-                    Check Ai Content
-                </button>
-            </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
-
 
 export default CreateBlog;
