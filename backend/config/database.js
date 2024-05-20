3d5979f787e0b49f4f660732b59b5046aa027b17
DevanshArora312 <devansharora312@gmail.com>
2024-05-20 15:18:09 +0530

    changed db file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose");
-
+require("dotenv").config();
 const dbconnect= ()=>{
-    mongoose.connect("mongodb://localhost:27017/bloggingplatform").then(()=>{
+    mongoose.connect(process.env.DB_URI).then(()=>{
         console.log("connected to databse");
     })
     .catch((err)=>{
